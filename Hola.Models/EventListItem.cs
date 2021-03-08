197126fdc9e7785a734e68deb679b85b8319187a
Julian <arboledajulian@outlook.com>
2021-03-08 16:13:19 -0500

    added lists and creates

---
+++ Hola.Models/EventListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola.Models
+{
+    public class EventListItem
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ Hola.Models/LocationListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola.Models
+{
+    public class LocationListItem
+    {
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
