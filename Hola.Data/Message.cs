3f3d026ca38476563981bc0eca5a052cc0a62b43
William casteel <will81656@gmail.com>
2021-03-09 14:56:24 -0600

    tables

--- Hola.Data/Message.cs
+++ Hola.Data/Message.cs
@@ -12,7 +12,7 @@ namespace Hola.Data
     {
         [Key]
         public int MessageId { get; set; }
-        [Required]
+       
         public Guid CreatorId { get; set; }
         [Required]
         public string Title { get; set; }
---
+++ Hola.Data/Migrations/202103092055513_updaa.cs
@@ -0,0 +1,16 @@
+namespace Hola.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updaa : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
