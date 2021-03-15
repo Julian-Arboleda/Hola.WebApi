a814dbbcd495be0796989b2d978cec04c192c647
Julian <arboledajulian@outlook.com>
2021-03-15 16:51:53 -0400

    added EventMessage

---
+++ Hola.Models/EventMessageCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola.Models
+{
+    public class EventMessageCreate
+    {
+        [Required]
+        [MaxLength(800, ErrorMessage = "There are too many characters in this field.")]
+        public string Content { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+    }
+}
---
+++ Hola.WebApi/Controllers/EventMessageController.cs
@@ -0,0 +1,46 @@
+﻿using Hola.Models;
+using Hola.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hola.WebApi.Controllers
+{
+    [Authorize]
+    public class EventMessageController : ApiController
+    {
+        private EventMessageService CreateEventMessageService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var eventMessageService = new EventMessageService(userId);
+            return eventMessageService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            EventMessageService eventMessageService = CreateEventMessageService();
+            var eventMessage = eventMessageService.GetEventMessages();
+            return Ok(eventMessage);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(EventMessageCreate eventMessage)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateEventMessageService();
+
+            if (!service.CreateEventMessage(eventMessage))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
+
